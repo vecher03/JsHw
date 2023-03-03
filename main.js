c28631fc487b82564c0fe0bd18c1eb8d953e6d5c
Ogohi <phoenix.cod4@mail.ru>
2023-03-03 10:53:10 +0500

    save

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+var imya = prompt ('Введите ваше имя')
+var age = prompt ('Введите ваш возраст')
+var primer1 = prompt ('Решите пример 10 + 5 = ')
+console.log('Мне ' + age);
+console.log('Меня зовут ' + imya);
+console.log('Пример 1; 10 + 5 = ' + (10 + 5) + ' Ваш ответ: ' + primer1 );
+var primer2 = prompt('Решите пример 10 - 5=? ')
+console.log('Пример 2; 10 - 5 = ' + (10 - 5) + ' Ваш ответ: ' + primer2 );
+var primer3 = prompt('Решите пример 10 * 5=? ')
+console.log('Пример 3; 10 * 5 = ' + (10 * 5) + ' Ваш ответ: ' + primer3 );
+var primer4 = prompt('Решите пример 10 / 5=? ')
+console.log('Пример 4; 10 / 5 = ' + (10 / 5) + ' Ваш ответ: ' + primer4 );
+var first = prompt( "Введите первое число")
+var second = prompt( "Введите второе число")
+var third = prompt( "Введите третье число")
+console.log(( "Среднее арифметическое: " + ( (first + second + third) / 3) ) );
